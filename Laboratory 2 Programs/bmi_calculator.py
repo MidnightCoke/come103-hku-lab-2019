dc4f2e3592c0617d5f453d6cf441e1a97585f967
efe <jimmyfallon717@gmail.com>
2019-10-18 11:46:03 +0300

    second lab has been finished

---
+++ Laboratory 2 Programs/bmi_calculator.py
@@ -0,0 +1,7 @@
+
+WEIGHT = float(input("Enter you're Weight: "))
+HEIGHT = float(input("Enter you're Height: "))
+
+BMI = WEIGHT / (HEIGHT**2)
+
+print("You're BMI is :", format(BMI, '.1f'))
---
+++ Laboratory 2 Programs/mpg_used.py
@@ -0,0 +1,8 @@
+
+MILES_DRIVEN = float(input("Enter the driven miles: "))
+
+USED_GAS = float(input("How many gas used?: "))
+
+MPG = MILES_DRIVEN / USED_GAS
+
+print("Car's MPG is: ", format(MPG, '.1f'))
